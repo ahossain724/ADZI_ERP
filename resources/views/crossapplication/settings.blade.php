f388e388f6e65522e55d90d71a6a6b94f4ee69b9
Ashek Hossain <ahossain724@hotmail.com>
2025-03-25 13:21:25 +0600

    project formatting updated

--- resources/views/crossapplication/settings.blade.php
+++ resources/views/crossapplication/settings.blade.php
@@ -18,50 +18,50 @@
 
     <!-- Main content -->
     <section class="content">
-        <div class="container-fluid">
-            <!-- SELECT2 EXAMPLE -->
-            <div class="card card-default w-100">
-                <div class="card-header">
-                    <h3 class="card-title">Defaults</h3>
-                    <div class="card-tools">
-                        <button type="button" class="btn btn-tool" data-card-widget="maximize">
-                            <i class="fas fa-expand"></i>
-                        </button>
-                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                            <i class="fas fa-minus"></i>
-                        </button>
-                    </div>
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="form-group">
-                                <label>Multiple</label>
-                                <select class="duallistbox w-100" multiple="multiple">
-                                    <option selected>Alabama</option>
-                                    <option>Alaska</option>
-                                    <option>California</option>
-                                    <option>Delaware</option>
-                                    <option>Tennessee</option>
-                                    <option>Texas</option>
-                                    <option>Washington</option>
-                                </select>
-                            </div>
-                            <!-- /.form-group -->
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                    <!-- /.row -->
-                </div>
 
+        <!-- SELECT2 EXAMPLE -->
+        <div class="card card-default w-100">
+            <div class="card-header">
+                <h3 class="card-title">Defaults</h3>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="maximize">
+                        <i class="fas fa-expand"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                </div>
             </div>
             <!-- /.card-header -->
-            <!-- /.card-body -->
-            <div class="card-footer">
-
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label>Multiple</label>
+                            <select class="duallistbox w-100" multiple="multiple">
+                                <option selected>Alabama</option>
+                                <option>Alaska</option>
+                                <option>California</option>
+                                <option>Delaware</option>
+                                <option>Tennessee</option>
+                                <option>Texas</option>
+                                <option>Washington</option>
+                            </select>
+                        </div>
+                        <!-- /.form-group -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
             </div>
+
         </div>
+        <!-- /.card-header -->
+        <!-- /.card-body -->
+        <div class="card-footer">
+
+        </div>
+
 
         <!-- /.card -->
 
@@ -1646,20 +1646,20 @@
 
                 </div>
                 <div class="row mt-4">
-                  <div class="col-md-2">
-                      <button type="button" class="btn btn-block btn-primary">Save</button>
-                  </div>
-                  <div class="col-md-2">
-                      <button type="button" class="btn btn-block btn-primary">Delete</button>
-                  </div>
-                  <div class="col-md-2">
-                      <button type="button" class="btn btn-block btn-primary">Refresh</button>
-                  </div>
-              </div>
+                    <div class="col-md-2">
+                        <button type="button" class="btn btn-block btn-primary">Save</button>
+                    </div>
+                    <div class="col-md-2">
+                        <button type="button" class="btn btn-block btn-primary">Delete</button>
+                    </div>
+                    <div class="col-md-2">
+                        <button type="button" class="btn btn-block btn-primary">Refresh</button>
+                    </div>
+                </div>
             </div>
             </br>
 
-            
+
             <div class="card-footer">
 
             </div>
@@ -1668,6 +1668,4 @@
 
         <!-- /.container-fluid -->
     </section>
-
-    
 @endsection
